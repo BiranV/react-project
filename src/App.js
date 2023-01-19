5c6e670b6fe1721eff5319b3621cadb13c0e55fa
biran varon <biraniv@gmail.com>
2023-01-19 20:00:42 +0200

    update

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {
   onSnapshot,
   doc,
   addDoc,
+  updateDoc,
   deleteDoc,
 } from "firebase/firestore";
 
@@ -12,10 +13,12 @@ function App() {
   const [recipes, setRecipes] = useState([]);
   const [form, setForm] = useState({
     title: "",
-    description: "",
+    desc: "",
     ingredients: [],
     steps: [],
   });
+  const [editActive, setEditActive] = useState(false);
+  const [editForm, setEditForm] = useState({});
   const [popupActive, setPopupActive] = useState(false);
 
   const recipesCollecionRef = collection(db, "recipes");
@@ -45,9 +48,18 @@ function App() {
     });
     setRecipes(recipeClone);
   };
+
+  const handleEdit = (id) => {
+    const recipeClone = [...recipes];
+    recipeClone.forEach((recipe, key) => {
+      if (recipe.id === id) {
+        setEditForm({ ...recipe });
+        setEditActive(true);
+      }
+    });
+  };
   const handleSubmit = (e) => {
     e.preventDefault();
-
     if (
       !form.title ||
       !form.desc ||
@@ -66,21 +78,35 @@ function App() {
       ingredients: [],
       steps: [],
     });
-
     setPopupActive(false);
   };
+  const handleEditSubmit = (e) => {
+    e.preventDefault();
+    if (
+      !editForm.title ||
+      !editForm.desc ||
+      editForm.ingredients.length < 1 ||
+      editForm.steps.length < 1
+    ) {
+      alert("Please fill out all fields");
+      return;
+    } else {
+      updateDoc(doc(db, "recipes", editForm.id), editForm);
+    }
+    setEditActive(false);
+  };
 
-  const handleIngredient = (e, i) => {
+  const handleIngredient = (e, index) => {
     const ingredientsClone = [...form.ingredients];
-    ingredientsClone[i] = e.target.value;
+    ingredientsClone[index] = e.target.value;
     setForm({
       ...form,
       ingredients: ingredientsClone,
     });
   };
-  const handleStep = (e, i) => {
+  const handleStep = (e, index) => {
     const stepssClone = [...form.steps];
-    stepssClone[i] = e.target.value;
+    stepssClone[index] = e.target.value;
     setForm({
       ...form,
       steps: stepssClone,
@@ -97,13 +123,37 @@ function App() {
   const removeRecipe = (id) => {
     deleteDoc(doc(db, "recipes", id));
   };
+  //from here
+  const handleEditIngredient = (e, index) => {
+    const ingredientsClone = [...editForm.ingredients];
+    ingredientsClone[index] = e.target.value;
+    setEditForm({
+      ...editForm,
+      ingredients: ingredientsClone,
+    });
+  };
+  const handleEditStep = (e, index) => {
+    const stepssClone = [...editForm.steps];
+    stepssClone[index] = e.target.value;
+    setEditForm({
+      ...form,
+      steps: stepssClone,
+    });
+  };
+  const handleEditIngredientCount = () => {
+    setEditForm({ ...editForm, ingredients: [...editForm.ingredients, ""] });
+  };
+
+  const handleEditStepCount = () => {
+    setEditForm({ ...editForm, steps: [...editForm.steps, ""] });
+  };
 
   return (
     <div className="App">
       <h1>My recipes</h1>
       <button onClick={() => setPopupActive(!popupActive)}>Add recipe</button>
       <div className="recipes">
-        {recipes.map((recipe, i) => (
+        {recipes.map((recipe, index) => (
           <div className="recipe" key={recipe.id}>
             <h3>{recipe.title}</h3>
             <p dangerouslySetInnerHTML={{ __html: recipe.desc }}></p>
@@ -111,21 +161,30 @@ function App() {
               <div>
                 <h4>Ingredients</h4>
                 <ul>
-                  {recipe.ingredients.map((ingredient, i) => (
-                    <li key={i}>{ingredient}</li>
+                  {recipe.ingredients.map((ingredient, index) => (
+                    <li key={index}>{ingredient}</li>
                   ))}
                 </ul>
                 <h4>Steps</h4>
                 <ol>
-                  {recipe.steps.map((step, i) => (
-                    <li key={i}>{step}</li>
+                  {recipe.steps.map((step, index) => (
+                    <li key={index}>{step}</li>
                   ))}
                 </ol>
               </div>
             )}
             <div className="buttons">
-              <button className="button-view" onClick={() => handleView(recipe.id)}>
+              <button
+                className="button-view"
+                onClick={() => handleView(recipe.id)}
+              >
                 View {recipe.viewing ? "less" : "more"}
+              </button>
+              <button
+                className="button-edit"
+                onClick={() => handleEdit(recipe.id)}
+              >
+                Edit
               </button>
               <button
                 className="remove"
@@ -160,38 +219,125 @@ function App() {
               </div>
               <div className="form-group">
                 <label>Ingredients</label>
-                {form.ingredients.map((ingredient, i) => (
+                {form.ingredients.map((ingredient, index) => (
                   <input
                     type="text"
-                    key={i}
+                    key={index}
                     value={ingredient}
-                    onChange={(e) => handleIngredient(e, i)}
+                    onChange={(e) => handleIngredient(e, index)}
                   />
                 ))}
-                <button type="button" className="button-view" onClick={handleIngredientCount}>
+                <button
+                  type="button"
+                  className="button-view"
+                  onClick={handleIngredientCount}
+                >
                   Add igredient
                 </button>
               </div>
               <div className="form-group">
                 <label>Steps</label>
-                {form.steps.map((step, i) => (
+                {form.steps.map((step, index) => (
                   <input
                     type="text"
-                    key={i}
+                    key={index}
                     value={step}
-                    onChange={(e) => handleStep(e, i)}
+                    onChange={(e) => handleStep(e, index)}
                   />
                 ))}
-                <button type="button" className="button-view" onClick={handleStepCount}>
+                <button
+                  type="button"
+                  className="button-view"
+                  onClick={handleStepCount}
+                >
                   Add step
                 </button>
               </div>
               <div className="buttons">
-                <button type="submit" className="submit">Submit</button>
+                <button type="submit" className="submit">
+                  Submit
+                </button>
                 <button
                   type="submit"
                   className="remove"
                   onClick={() => setPopupActive(false)}
+                >
+                  Close
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      )}
+      {editActive && (
+        <div className="popup">
+          <div className="popup-inner">
+            <h2>Edit recipe</h2>
+            <form onSubmit={handleEditSubmit}>
+              <div className="form-group">
+                <label>Title</label>
+                <input
+                  type="text"
+                  value={editForm.title}
+                  onChange={(e) =>
+                    setEditForm({ ...editForm, title: e.target.value })
+                  }
+                />
+              </div>
+              <div className="form-group">
+                <label>Description</label>
+                <textarea
+                  type="text"
+                  value={editForm.desc}
+                  onChange={(e) =>
+                    setEditForm({ ...editForm, desc: e.target.value })
+                  }
+                />
+              </div>
+              <div className="form-group">
+                <label>Ingredients</label>
+                {editForm.ingredients.map((ingredient, index) => (
+                  <input
+                    type="text"
+                    key={index}
+                    value={ingredient}
+                    onChange={(e) => handleEditIngredient(e, index)}
+                  />
+                ))}
+                <button
+                  type="button"
+                  className="button-view"
+                  onClick={handleEditIngredientCount}
+                >
+                  Add igredient
+                </button>
+              </div>
+              <div className="form-group">
+                <label>Steps</label>
+                {editForm.steps.map((step, index) => (
+                  <input
+                    type="text"
+                    key={index}
+                    value={step}
+                    onChange={(e) => handleEditStep(e, index)}
+                  />
+                ))}
+                <button
+                  type="button"
+                  className="button-view"
+                  onClick={handleEditStepCount}
+                >
+                  Add step
+                </button>
+              </div>
+              <div className="buttons">
+                <button type="submit" className="submit">
+                  Submit
+                </button>
+                <button
+                  type="submit"
+                  className="remove"
+                  onClick={() => setEditActive(false)}
                 >
                   Close
                 </button>
