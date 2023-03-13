7839363c3cd27a326d119fcfd4eafe4acc73bb8c
biran varon <biraniv@gmail.com>
2023-03-13 10:19:19 +0200

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,9 @@
 import { NavLink } from "react-router-dom"
+import Image from "./preview1.png"
 
 export default function TheHeader() {
     return (
-        <nav className="header" >
+        <nav className="header" style={{ backgroundImage: `url(${Image})` ,backgroundSize: 'cover'}}>
             <NavLink to="/"><h1>React Project - Biran Varon</h1></NavLink>
             <div className="nav-container">
                 <NavLink className="nav-page" style={({ isActive }) => isActive ? { border: '1px solid #ffffff' } : null} to="/recipes">Recipes</NavLink>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,14 +10,23 @@ export default function Home() {
                     <br />
                     <p style={{ textDecoration: "underline", fontSize: "22px" }}>Technologies included:</p>
                     React, React Router, Redux, Local Storage and Firebase.
-                </div>
+                    <br/>  </div>
+            </div>
+            <div className="home-container-pages">
+                <div className="home-container" style={{marginRight: '0.25rem'}}><h2>Recipes</h2>
+                    <h4>An app for recipes</h4></div>
+                <div className="home-container" style={{marginLeft: '0.25rem'}}><h2>Expenses</h2>
+                    <h4>An application for calculating expenses</h4>
+                    <br/>
+                    </div>
             </div>
             <div className="home-container">
                 <div style={{ fontWeight: "bold", fontSize: "20px" }}>
                     Created by: Biran Varon.
-                    <p><a href="https://github.com/BiranV" target="_blank" rel="noreferrer" > <button className="add" >GitHub</button></a></p>
+                    <p><a href="https://github.com/BiranV" target="_blank" rel="noreferrer" style={{ textDecoration: 'none' }}> <button className="add" >GitHub</button></a></p>
                 </div>
             </div>
+
         </header>
     )
 }
