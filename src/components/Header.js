60cb1ed1535a2202893aaaec09ac1fe0e4bbba56
biran varon <biraniv@gmail.com>
2023-03-13 13:20:20 +0200

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,11 +3,11 @@ import Image from "./preview1.png"
 
 export default function TheHeader() {
     return (
-        <nav className="header" style={{ backgroundImage: `url(${Image})` ,backgroundSize: 'cover'}}>
-            <NavLink to="/"><h1>React Project - Biran Varon</h1></NavLink>
+        <nav className="header" style={{ backgroundImage: `url(${ Image })`, backgroundSize: 'cover' }}>
+            <NavLink to="/"><h1 className="title">React Project - Biran Varon</h1></NavLink>
             <div className="nav-container">
-                <NavLink className="nav-page" style={({ isActive }) => isActive ? { border: '1px solid #ffffff' } : null} to="/recipes">Recipes</NavLink>
-                <NavLink className="nav-page" style={({ isActive }) => isActive ? { border: '1px solid #ffffff' } : null} to="/expenses">Expenses</NavLink>
+                <NavLink className="nav-page" activeClassName="active" to="/recipes">Recipes</NavLink>
+                <NavLink className="nav-page" activeClassName="active" to="/expenses">Expenses</NavLink>
             </div>
         </nav>
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,32 +1,28 @@
 export default function Home() {
     return (
-        <header className="home-app">
-            <div className="home-container">
-                <h1 style={{ textDecoration: "underline" }}>Welcome to my React project</h1>
-                <div style={{ fontSize: "18px" }}>Here you can find two apps: a recipe book and an expense calculator.
+        <div className="home-app">
+            <div className="card">
+                <h2 className="title" style={{ textDecoration: "underline" }}>Welcome to my React project</h2>
+                <p>Here you can find two apps: a recipe book and an expense calculator.
+                    <p>Each of the apps can be accessed via a navigation bar at the top. </p>
                     <br />
-                    Each of the apps can be accessed via a navigation bar at the top.
-                    <br />
-                    <br />
-                    <p style={{ textDecoration: "underline", fontSize: "22px" }}>Technologies included:</p>
-                    React, React Router, Redux, Local Storage and Firebase.
-                    <br/>  </div>
+                    <p>Technologies included:</p>
+                    <p>React, React Router, Redux, Local Storage and Firebase.</p>
+                </p>
             </div>
-            <div className="home-container-pages">
-                <div className="home-container" style={{marginRight: '0.25rem'}}><h2>Recipes</h2>
-                    <h4  style={{marginTop: '1.5rem'}}>An app for recipes</h4></div>
-                <div className="home-container" style={{marginLeft: '0.25rem'}}><h2>Expenses</h2>
-                    <h4 style={{marginTop: '1.5rem'}}>An application for calculating expenses</h4>
-                    <br/>
-                    </div>
-            </div>
-            <div className="home-container">
-                <div style={{ fontWeight: "bold", fontSize: "20px" }}>
-                    Created by: Biran Varon.
-                    <p><a href="https://github.com/BiranV" target="_blank" rel="noreferrer" style={{ textDecoration: 'none' }}> <button className="add" >GitHub</button></a></p>
+            <div className="cards-flex">
+                <div className="card" style={{ marginRight: '0.25rem' }}><h2>Recipes</h2>
+                    <h4 style={{ marginTop: '1.5rem' }}>An app for recipes</h4></div>
+                <div className="card" style={{ marginLeft: '0.25rem' }}><h2>Expenses</h2>
+                    <h4 style={{ marginTop: '1.5rem' }}>An application for calculating expenses</h4>
                 </div>
             </div>
+            <div className="card">
+                <h3>Full Name: Biran Varon</h3>
+                <h3>Mobile: 0507780228</h3>
+                <a href="https://github.com/BiranV" target="_blank" rel="noreferrer" style={{ textDecoration: 'none' }}> <button className="add" >GitHub</button></a>
+            </div>
 
-        </header>
+        </div>
     )
 }
